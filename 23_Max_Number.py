cd123efcc6d3bf24016be024ab60765f539e5b6a
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-23 18:59:41 +0530

    Maximum number

---
+++ 23_Max_Number.py
@@ -0,0 +1,8 @@
+def maximum_number(a, b, c):
+    return max(a, b, c)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Maximum number is: ", max(a, b, c))
