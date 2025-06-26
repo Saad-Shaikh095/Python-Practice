726b6e56d9fb6d608802604fedce0411b4fd45fe
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-26 14:39:59 +0530

    Vowels

---
+++ 27_Vowels.py
@@ -0,0 +1,10 @@
+def check_vowels(s):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+string = input("Enter a string: ")
+print(check_vowels(string))
