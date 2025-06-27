3f0d93b8acea08ea32f521b8eec93f3996b50f8c
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-27 19:05:26 +0530

    Tupples

---
+++ 31_Tupples.py
@@ -0,0 +1,12 @@
+# CReating a tupple 
+my_tupple = ("BMW", "Rolls Royce", "Porsche")
+print(my_tupple)
+
+# This will only print the index value which is mentioned
+my_tupple1 = ("BMW", "Rolls Royce", "Porsche")
+print(my_tupple1[1])
+
+# This will show error as tuppls are fixed, we cannot change it further 
+my_tupple2 = ("BMW", "Rolls Royce", "Porsche")
+my_tupple2[2] = "Mercedes"
+print(my_tupple2)
