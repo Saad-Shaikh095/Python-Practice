6fdc143cf727fa537efff84a7c60bff669bde1f8
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-27 18:58:26 +0530

    Lists

---
+++ 30_List.py
@@ -0,0 +1,21 @@
+# Creating a List
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+# Appending the list, means adding a number 
+my_list.append(60)
+print(my_list)
+
+# Inserting a number at a specific index
+my_list.insert(3, 100)
+print(my_list)
+
+# Sorting the list in smallest to biggest 
+my_list2 = [5, 3, 6, 9, 1, 2]
+my_list2.sort()
+print("Sorted", my_list2)
+
+# Removing an element for the list
+cars = ["BMW", "Rolls Royce", "Mercedes", "Porsche"]
+cars.remove("Mercedes")
+print(cars)
