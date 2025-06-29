40931e76a9ec2d3e12c527f402287654729ea8f8
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-29 13:47:08 +0530

    Question 02

--- practice Questions/Question_02.py
+++ practice Questions/Question_02.py
@@ -6,9 +6,11 @@ Given a tuple my_tuple = (10, 20, 30, 40, 50), write Python code to:
 
 my_tupple = (10, 20, 30, 40, 50)
 
+# 1.
 print("First: ", my_tupple[0])
 print("Last: ", my_tupple[4])
 
+# 2. 
 my_list = list(my_tupple)
 my_list.append(60)
 print(my_list)
