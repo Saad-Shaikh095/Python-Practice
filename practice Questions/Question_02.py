342a602d20e213ce2116610023174a4e9eaab293
Saad Shaikh <saadshaikh400695@gmail.com>
2025-06-29 13:46:26 +0530

    Question 01

---
+++ practice Questions/Question_02.py
@@ -0,0 +1,14 @@
+'''
+Given a tuple my_tuple = (10, 20, 30, 40, 50), write Python code to:
+    1.  Print the first and last elements.
+    2. Convert it into a list and add the number 60.   
+'''
+
+my_tupple = (10, 20, 30, 40, 50)
+
+print("First: ", my_tupple[0])
+print("Last: ", my_tupple[4])
+
+my_list = list(my_tupple)
+my_list.append(60)
+print(my_list)
